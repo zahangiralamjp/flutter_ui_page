648064f176e25597b05782071732afa8bc1bcb6f
shaplaflowers <thezahangir@gmail.com>
2022-01-29 08:43:57 +0600

    Positioned Stack Multi-Container BorderRadius.circular

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class FluterUI extends StatelessWidget {
                   width: 500,
                   height: 600,
                   color: Colors.amber,
-/////////// Positioned Stack Container BorderRadius.circular //////////////////////////////
+/////////// Positioned Stack Multi-Container BorderRadius.circular //////////////////////////////
                   child: Stack(
                     children: [
                       Positioned(
@@ -45,46 +45,134 @@ class FluterUI extends StatelessWidget {
                             ),
                           )),
                       Positioned(
-                          right: 150,
-                          top: 20,
+                          left: 50,
+                          top: 180,
                           child: Container(
-                            width: 100,
-                            height: 100,
+                            width: 30,
+                            height: 30,
                             decoration: BoxDecoration(
-                              color: Colors.white,
+                              color: Colors.brown,
                               borderRadius: BorderRadius.circular(50),
                             ),
                           )),
                       Positioned(
-                          right: 50,
-                          top: 200,
+                          right: 150,
+                          top: 30,
+                          child: Container(
+                            width: 70,
+                            height: 70,
+                            decoration: BoxDecoration(
+                              color: Colors.brown,
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          )),
+                      Positioned(
+                          left: 150,
+                          bottom: 130,
                           child: Container(
                             width: 100,
                             height: 100,
                             decoration: BoxDecoration(
-                              color: Colors.cyan,
+                              color: Colors.brown,
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          )),
+                      Positioned(
+                          left: 250,
+                          top: 130,
+                          child: Container(
+                            width: 90,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              color: Colors.brown,
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          )),
+                      Positioned(
+                          left: 60,
+                          top: 200,
+                          child: Container(
+                            width: 90,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              color: Colors.brown,
                               borderRadius: BorderRadius.circular(50),
                             ),
                           )),
                       Positioned(
                           right: 200,
+                          bottom: 240,
+                          child: Container(
+                            width: 150,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              color: Colors.brown,
+                              borderRadius: BorderRadius.circular(90),
+                            ),
+                          )),
+                      Positioned(
                           bottom: 50,
+                          left: 30,
                           child: Container(
                             width: 100,
                             height: 100,
                             decoration: BoxDecoration(
-                              color: Colors.red,
+                              color: Colors.brown,
                               borderRadius: BorderRadius.circular(50),
                             ),
                           )),
                       Positioned(
-                          left: 50,
-                          top: 210,
+                          bottom: 60,
+                          right: 30,
                           child: Container(
-                            width: 100,
-                            height: 100,
+                            width: 30,
+                            height: 30,
                             decoration: BoxDecoration(
-                              color: Colors.redAccent,
+                              color: Colors.brown,
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          )),
+                      Positioned(
+                          bottom: 70,
+                          right: 100,
+                          child: Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              color: Colors.brown,
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          )),
+                      Positioned(
+                          bottom: 150,
+                          right: 90,
+                          child: Container(
+                            width: 90,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              color: Colors.brown,
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          )),
+                      Positioned(
+                          bottom: 260,
+                          right: 60,
+                          child: Container(
+                            width: 30,
+                            height: 30,
+                            decoration: BoxDecoration(
+                              color: Colors.brown,
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          )),
+                      Positioned(
+                          top: 150,
+                          right: 60,
+                          child: Container(
+                            width: 90,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              color: Colors.brown,
                               borderRadius: BorderRadius.circular(50),
                             ),
                           )),
