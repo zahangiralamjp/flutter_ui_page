c582cd96cc13c03b99d697da211b114e2370d81f
shaplaflowers <thezahangir@gmail.com>
2022-01-30 13:09:58 +0600

    Facebook Log In Page UI

--- lib/main.dart
+++ lib/main.dart
@@ -9,115 +9,151 @@ void main(List<String> args) {
   ));
 }
 
-//////////// Container WITH Transform /////////////////////////
+//////////// Log IN UI /////////////////////////
 class FluterUI extends StatelessWidget {
+  const FluterUI({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(' - Stack UI - '),
-        centerTitle: true,
-      ),
+      backgroundColor: Colors.black26,
       body: Center(
-        // child: Transform.rotate(
-        //   angle: 1,
-        //   child: Container(
-        //     width: 100,
-        //     height: 100,
-        //     color: Colors.amber,
-        //   ),
-        // ),
         child: Column(
           children: [
-            Container(
-              width: 500,
-              height: 500,
-              color: Colors.amber,
-              child: Stack(
-                children: [
-                  Positioned(
-                      left: 30,
-                      top: 30,
-                      child: Transform.rotate(
-                        angle: 1,
-                        child: Container(
-                          width: 100,
-                          height: 100,
-                          color: Colors.white,
-                        ),
-                      )),
-                  Positioned(
-                      top: 260,
-                      left: 150,
-                      child: Transform.rotate(
-                          angle: 2,
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            color: Colors.white,
-                          ))),
-                  Positioned(
-                      bottom: 50,
-                      right: 130,
-                      child: Transform.rotate(
-                          angle: 2,
-                          child: Container(
-                            width: 120,
-                            height: 200,
-                            color: Colors.white,
-                          ))),
-                  Positioned(
-                      top: 150,
-                      right: 130,
-                      child: Transform.rotate(
-                          angle: 2,
-                          child: Container(
-                            width: 90,
-                            height: 90,
-                            color: Colors.white,
-                          ))),
-                  Positioned(
-                      bottom: 30,
-                      left: 130,
-                      child: Transform.rotate(
-                          angle: 2,
-                          child: Container(
-                            width: 90,
-                            height: 90,
-                            color: Colors.white,
-                          ))),
-                  Positioned(
-                      top: 40,
-                      right: 50,
-                      child: Transform.rotate(
-                          angle: 2,
-                          child: Container(
-                            width: 90,
-                            height: 90,
-                            color: Colors.white,
-                          ))),
-                  Positioned(
-                      top: 40,
-                      right: 190,
-                      child: Transform.rotate(
-                          angle: 2,
-                          child: Container(
-                            width: 50,
-                            height: 60,
-                            color: Colors.white,
-                          ))),
-                  Positioned(
-                      top: 200,
-                      left: 50,
-                      child: Transform.rotate(
-                          angle: 2,
-                          child: Container(
-                            width: 100,
-                            height: 100,
-                            color: Colors.white,
-                          ))),
-                ],
-              ),
+            SizedBox(
+              height: 90,
+            ),
+            ///////////////////////////////
+            Text(
+              'facebook',
+              style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            //////////////////////////////
+            Stack(
+              children: [
+                ///////// Stack Box //////////////////////
+                Container(
+                  width: 450,
+                  height: 350,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                ///////////////////////////////
+                Positioned(
+                    left: 5,
+                    right: 5,
+                    top: 5,
+                    bottom: 5,
+                    child: Container(
+                      width: 430,
+                      height: 330,
+                      color: Colors.white10,
+                      child: Column(
+                        children: [
+                          //////////// Form Start /////////////////
+
+                          Container(
+                            child: Padding(
+                              padding: const EdgeInsets.all(25.0),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Log in to Facebook',
+                                    style: TextStyle(fontSize: 25),
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  TextField(
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      hintText: 'Email address or phone number',
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  TextField(
+                                    obscureText: true,
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      hintText: 'Enter Password',
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  MaterialButton(
+                                    minWidth: double.infinity,
+                                    height: 50,
+                                    splashColor: Colors.black,
+                                    color: Colors.blue,
+                                    onPressed: () {},
+                                    shape: RoundedRectangleBorder(
+                                      side: BorderSide(
+                                        color: Colors.blue,
+                                      ),
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                    child: Text(
+                                      'Log In',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 40,
+                                  ),
+                                  Container(
+                                    child: Center(
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            'Forgotten account?',
+                                            style:
+                                                TextStyle(color: Colors.blue),
+                                          ),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            'Sign up for Facebook',
+                                            style:
+                                                TextStyle(color: Colors.blue),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+
+///////////////////////Form END ///////////////////////////////
+                        ],
+                      ),
+                    )),
+                ///////////////////////////////
+              ],
             ),
           ],
         ),
